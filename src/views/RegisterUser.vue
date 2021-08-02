28327a3c48abbabfd2a6b6cbedbabdd8c29b53f4
Ali MOHAMED MOUSSA <alimohamed.moussa@gmail.com>
2021-08-02 22:30:12 +0200

    initial commit

--- src/views/RegisterUser.vue
+++ src/views/RegisterUser.vue
@@ -4,17 +4,17 @@
       <label for="name">
         Name:
       </label>
-      <input v-model="name" type="text" name="name" value>
+      <input v-model="name" type="text" name="name" value />
 
       <label for="email">
         Email:
       </label>
-      <input v-model="email" type="email" name="email" value>
+      <input v-model="email" type="email" name="email" value />
 
       <label for="password">
         Password:
       </label>
-      <input v-model="password" type="password" name value>
+      <input v-model="password" type="password" name value />
 
       <button type="submit" name="button">
         Register
@@ -33,31 +33,31 @@
 
 <script>
 export default {
-  data () {
+  data() {
     return {
-      name: '',
-      email: '',
-      password: '',
-      errors: null
-    }
+      name: "",
+      email: "",
+      password: "",
+      errors: null,
+    };
   },
 
   methods: {
-    register () {
+    register() {
       this.$store
-        .dispatch('register', {
+        .dispatch("register", {
           name: this.name,
           email: this.email,
-          password: this.password
+          password: this.password,
         })
         .then(() => {
-          this.$router.push({ name: 'dashboard' })
+          this.$router.push({ name: "dashboard" });
         })
         .catch((err) => {
-          this.errors = err.response.data.errors
-        })
-    }
-  }
-}
+          this.errors = err.response.data.errors;
+        });
+    },
+  },
+};
 </script>
 <style scoped></style>
